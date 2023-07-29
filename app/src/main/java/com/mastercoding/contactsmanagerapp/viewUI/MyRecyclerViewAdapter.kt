6f8e845e1928cf546001c884977f95e6ce7536f0
BRIAN MUTEBI <33450898+mutebibrian@users.noreply.github.com>
2023-07-29 12:18:17 +0300

    Update MyRecyclerViewAdapter.kt

--- app/src/main/java/com/mastercoding/contactsmanagerapp/viewUI/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/mastercoding/contactsmanagerapp/viewUI/MyRecyclerViewAdapter.kt
@@ -10,12 +10,17 @@ import com.mastercoding.contactsmanagerapp.databinding.CardItemBinding
 import com.mastercoding.contactsmanagerapp.room.User
 
 class MyRecyclerViewAdapter
+// on below line we are creating a
+    // variable for our all userslist.
     (private val usersList:List<User>,
         private val clickListener: (User)->Unit
      ) : RecyclerView.Adapter<MyViewHolder>()
 
 {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+                // inflating our layout file for each item of recycler view.
+
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding : CardItemBinding = DataBindingUtil.
                 inflate(layoutInflater, R.layout.card_item, parent, false)
@@ -23,15 +28,21 @@ class MyRecyclerViewAdapter
     }
 
     override fun getItemCount(): Int {
+        // on below line we are
+        // returning our list size.
         return usersList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+// on below line we are adding click listener
+        // to our recycler view item.
         holder.bind(usersList[position],clickListener)
     }
 
 
 }
+
+        // on below line we are creating a view holder class.
 
 class MyViewHolder(val binding: CardItemBinding):RecyclerView.ViewHolder(binding.root){
 
