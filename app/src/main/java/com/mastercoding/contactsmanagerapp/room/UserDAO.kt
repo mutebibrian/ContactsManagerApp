78cb83910812cb25f697cd0fe1a6bada315cb2e3
BRIAN MUTEBI <33450898+mutebibrian@users.noreply.github.com>
2023-07-29 11:45:58 +0300

    Update UserDAO.kt

--- app/src/main/java/com/mastercoding/contactsmanagerapp/room/UserDAO.kt
+++ app/src/main/java/com/mastercoding/contactsmanagerapp/room/UserDAO.kt
@@ -7,20 +7,36 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
+//annotation for dao class.
+
 @Dao
 interface UserDAO {
+    // below is the insert method for
+    // adding a new entry to our database.
 
     @Insert
     suspend fun insertUser(user: User):Long
 
+    // below is the update method
+    // for deleting our details.
     @Update
     suspend fun updateUser(user: User)
-
+    
+    
+// below is the delete method
+    // for deleting our details.
     @Delete
     suspend fun deleteUser(user: User)
+    
 
     @Query("DELETE FROM user")
     suspend fun deleteAll()
+
+    // below is the method to read all contact details
+    // from our database we have specified the query for it.
+    // order on below line and we are specifying
+    // the table name from which
+    // we have to get the data from.
 
     @Query("SELECT * FROM user")
     fun getAllUsersInDB(): LiveData<List<User>>
