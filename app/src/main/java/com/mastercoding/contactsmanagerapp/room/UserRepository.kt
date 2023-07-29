0da7cdf6104166e82ffd693c037eac53c5078441
BRIAN MUTEBI <33450898+mutebibrian@users.noreply.github.com>
2023-07-29 11:57:23 +0300

    Update UserRepository.kt

--- app/src/main/java/com/mastercoding/contactsmanagerapp/room/UserRepository.kt
+++ app/src/main/java/com/mastercoding/contactsmanagerapp/room/UserRepository.kt
@@ -3,18 +3,26 @@ package com.mastercoding.contactsmanagerapp.room
 class UserRepository(private val dao: UserDAO) {
 
     val users = dao.getAllUsersInDB()
+    // on below line we are creating an insert method
+    // for adding the user to our database.
 
     suspend fun insert(user: User): Long{
         return dao.insertUser(user)
     }
+    // on below line we are creating a delete method
+    // for deleting our user from database.
 
     suspend fun delete(user: User) {
         return dao.deleteUser(user)
     }
+    // on below line we are creating a update method
+    // for deleting our user from database.
     suspend fun update(user: User) {
         return dao.updateUser(user)
     }
 
+    // on below line we are creating a deleteAll method
+    // for deleting all our users from database.
     suspend fun deleteAll(){
         return dao.deleteAll()
     }
